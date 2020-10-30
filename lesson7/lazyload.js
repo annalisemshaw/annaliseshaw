8ff46d5d93ad0409069c7857a0fbeb6f9b6efe03
annalisemshaw <sha19023@byui.edu>
2020-10-29 22:39:00 -0700

    index push

--- lesson7/lazyload.js
+++ lesson7/lazyload.js
@@ -1,31 +1,36 @@
-const imagesToLoad = document.querySelectorAll("[data-src]");
+const imagesToLoad = document.querySelectorAll("img[data-src]");
 
-function preloadImage(img) {
-    const src = img.getAttribute("data-src");
-    if (!src) {
-        return;
-    }
-    img.src = src;
-}
+
 
 const imgOptions = {
-    threshold: 1,
-    rootMargin: "0px 0px -500px 0px"
+    threshold: 0.5
 };
 
-const imgObserver = IntersectionObserver((entries,
-    imgObserver) => {
-        entries.forEach(entry => {
-            if (!entry.isIntersecting) {
-                return;
-            } else {
-                preloadImage(entry.target);
-                imgObserver.unobserve(entry.target);
-            }
-        })
-    }, imgOptions);
+const loadImages = (image) => {
+    image.setAttribute('src', image.getAttribute('data-src'));
+    image.onload = () => {
+    image.removeAttribute("data-src");
+    };
+};
 
-imagesToLoad.forEach(image => {
-    imgObserver.observe(image);
+
+if ('IntersectionObserver' in window) {
+    const imgObserver = new IntersectionObserver ((items, imgObserver) => {
+    items.forEach((item) => {
+        if (item.isIntersecting){
+            loadImages(item.target);
+            imgObserver.unobserve(item.target);
+        }
+    });
+},imgOptions);
+
+
+imagesToLoad.forEach((img) => {
+    imgObserver.observe(img);
+    });
+} 
+else {
+imagesToLoad.forEach((img) => {
+loadImages(img);
 });
-
+}
