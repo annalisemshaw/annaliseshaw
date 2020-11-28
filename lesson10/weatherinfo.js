da3b3d2c3aa19ba3a4b1087421130411cd700758
annalisemshaw <sha19023@byui.edu>
2020-11-27 21:38:04 -0800

    com/push

--- lesson10/weatherinfo.js
+++ lesson10/weatherinfo.js
@@ -1,4 +1,4 @@
-const apiURL = "http://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&APPID=37729291edfa73e5654bc594532a508c";
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&APPID=37729291edfa73e5654bc594532a508c";
 fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {
