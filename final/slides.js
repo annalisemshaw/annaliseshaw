77d0409971ca2c203f5975235af036af47d6ac82
annalisemshaw <sha19023@byui.edu>
2020-12-16 13:53:21 -0800

    com/push

--- final/slides.js
+++ final/slides.js
@@ -8,7 +8,7 @@ function showSlides() {
     slides[i].style.display = "none";  
   }
   slideIndex++;
-  if (slideIndex >= slides.length) {slideIndex = 1}    
+  if (slideIndex > slides.length) {slideIndex = 1}    
   slides[slideIndex-1].style.display = "block";  
   setTimeout(showSlides, 2000); // Change image every 3 seconds
 }
